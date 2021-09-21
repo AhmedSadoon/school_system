b90b066e21ffdb48ffb69a6ca1fe591bad393938
AhmedSadoon <ahmedsadoon65@yahoo.com>
2021-09-21 19:39:27 +0300

    Update and Delete Grades

--- app/Http/Controllers/Web/Grade/GradeController.php
+++ app/Http/Controllers/Web/Grade/GradeController.php
@@ -49,6 +49,41 @@ class GradeController extends Controller
     }
 
 
+    public function update(StoreGradeRequest $request)
+    {
+      try {
+          $validated = $request->validated();
+          $Grades = Grade::findOrFail($request->id);
+          $Grades->update([
+            $Grades->Name = ['ar' => $request->Name, 'en' => $request->Name_en],
+            $Grades->Notes = $request->Notes,
+          ]);
+          toastr()->success(trans('messages.Update'));
+          return redirect()->route('grade.index');
+      }
+      catch
+      (\Exception $e) {
+          return redirect()->back()->withErrors(['error' => $e->getMessage()]);
+      }
+
+    }
+
+     /**
+      * Remove the specified resource from storage.
+      *
+      * @param  int  $id
+      * @return Response
+      */
+     public function destroy(Request $request)
+     {
+
+       $Grades = Grade::findOrFail($request->id)->delete();
+       toastr()->error(trans('messages.Delete'));
+       return redirect()->route('grade.index');
+
+     }
+
+
 
 
 
